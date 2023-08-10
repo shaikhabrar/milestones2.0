5b9d135b2c1e17c302c024c4664b92b63a594b70
abrar <shaikhabrar637@gmail.com>
2023-08-10 23:33:45 +0530

    updated url

--- frontend/src/config/development.js
+++ frontend/src/config/development.js
@@ -1,4 +1,4 @@
-const DOMAIN = "https://my-cloud-run-frontend-v2wk62suia-uc.a.run.app:9002"
+const DOMAIN = "https://my-cloud-run-frontend-v2wk62suia-uc.a.run.app:8080"
 export const API = {
   LOGIN: `${DOMAIN}/login`,
   SIGNUP: `${DOMAIN}/signup`,
