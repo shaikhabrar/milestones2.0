09cf422d2082d589e19a3530fff3e6195dd1299c
abrar <shaikhabrar637@gmail.com>
2023-08-10 23:56:19 +0530

    updated url

--- frontend/src/config/production.js
+++ frontend/src/config/production.js
@@ -1,5 +1,5 @@
 // const DOMAIN ="http://34.125.127.98:9002"
-const DOMAIN ="https://my-cloud-run-frontend-v2wk62suia-uc.a.run.app:8080"
+const DOMAIN ="https://my-cloud-run-frontend-v2wk62suia-uc.a.run.app"
 // const DOMAIN ="http://localhost:3000"
 export const API={
     LOGIN:`${DOMAIN}/login`,
